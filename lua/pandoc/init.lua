6857e64a4ad8adfedcdf4552890187742f2ca61d
Dilitha Wijayapala <diliw101@gmail.com>
2024-09-06 09:37:25 +1200

    Squash merge feat-fix-file-naming into main

--- lua/pandoc/init.lua
+++ lua/pandoc/init.lua
@@ -1,9 +1,44 @@
 local M = {}
 
+local DefaultConfig = {
+	PDF = {
+		MarginLeft = "2.5cm",
+		MarginRight = "2.5cm",
+		MarginTop = "2.5cm",
+		MarginBottom = "2.5cm",
+	},
+}
+
+local function strip_extension(file_name)
+	local index_of_dot = string.find(file_name, ".[^.]*$")
+	if index_of_dot == nil then
+		return file_name
+	end
+	return string.sub(file_name, 1, index_of_dot - 1)
+end
+
 local function render_pdf()
 	local buf_name = vim.fn.expand("%:p")
-	local pandoc_cmd = "pandoc -s -o " .. buf_name .. ".pdf " .. buf_name
-	vim.fn.system(pandoc_cmd)
+	local buf_minus_ext = strip_extension(buf_name)
+	local pandoc_cmd = "pandoc -s "
+		.. buf_name
+		.. " -o "
+		.. buf_minus_ext
+		.. ".pdf"
+		.. ' -V geometry:"left='
+		.. DefaultConfig.PDF.MarginLeft
+		.. ",right="
+		.. DefaultConfig.PDF.MarginRight
+		.. ",top="
+		.. DefaultConfig.PDF.MarginTop
+		.. ",bottom="
+		.. DefaultConfig.PDF.MarginBottom
+		.. '"'
+	local output = vim.fn.system(pandoc_cmd)
+	if output ~= "" then
+		print("Error: " .. output)
+		return
+	end
 	print("PDF generated")
 end
 
